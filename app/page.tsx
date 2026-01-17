ced344a0023b8c359a51190c84648b5491dfae22
zym <1039230702@qq.com>
2026-01-17 14:28:38 +0800

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
             Select Your Conflict Type
           </h3>
           <p className="text-neutral-600">
-            Choose the issue you'd like to discuss with your partner
+            Choose the issue you&apos;d like to discuss with your partner
           </p>
         </div>
         
@@ -125,7 +125,7 @@ export default function Home() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                   </svg>
-                  Partner's Personality
+                  Partner&apos;s Personality
                 </span>
               </label>
               <select 
